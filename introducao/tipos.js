c1f2dc74c00e06cbad75082daa8c47440bcd2df6
sdavidbastos <davidtavares98@gmail.com>
2020-05-26 03:13:24 -0300

    Array e tuplas

--- introducao/tipos.js
+++ introducao/tipos.js
@@ -1,4 +1,18 @@
 "use strict";
 var nome = "joão";
-console.log(nome);
-nome = 28;
+// console.log(nome)
+// nome = 28
+// Tipos explícitos
+var minhaIdade;
+minhaIdade = 27;
+// console.log(typeof minhaIdade)
+// minhaIdade = "idade é 27"
+// console.log(typeof minhaIdade)
+// array
+var hobbies = ['cozinhar', 'praticar', 'esportes'];
+// console.log(hobbies[0])
+// console.log(typeof hobbies)
+hobbies = [100, 200, 300];
+// tuplas
+var endereco = ["Av Principal", 99];
+console.log(endereco);
--- introducao/tipos.ts
+++ introducao/tipos.ts
@@ -1,4 +1,26 @@
 let nome = "joão"
-console.log(nome)
+// console.log(nome)
+// nome = 28
 
-nome = 28
+// Tipos explícitos
+let minhaIdade: number
+
+minhaIdade = 27
+
+// console.log(typeof minhaIdade)
+
+// minhaIdade = "idade é 27"
+// console.log(typeof minhaIdade)
+
+// array
+let hobbies: any[] = ['cozinhar', 'praticar', 'esportes']
+// console.log(hobbies[0])
+// console.log(typeof hobbies)
+
+hobbies = [100, 200, 300]
+
+// tuplas
+
+let endereco: [string, number] = ["Av Principal", 99]
+
+console.log(endereco)
